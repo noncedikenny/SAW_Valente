9ddc78b8bc97e4f48994e62bcf48abec2a53c2f3
noncedikenny <emanuelegmvalente@libero.it>
2024-05-15 10:46:17 +0200

    14 - Bug grafici risolti

--- private_area.php
+++ private_area.php
@@ -20,7 +20,7 @@
 <?php include('header.php'); ?>
 
 <main>
-    <div class="w3-card options-container">
+    <div class="w3-card w3-padding-32 options-container">
         <?php
             echo "<h1 class='w3-center' style='padding: 10px;'>Benvenuto {$_SESSION['firstname']} {$_SESSION['lastname']}</h1>"; ?>
 
@@ -28,10 +28,36 @@
             <nav>
                 <a href="#" style="text-decoration: none">Notifiche</a> |
                 <a href="#" style="text-decoration: none">Lingua</a> |
-                <a href="#" style="text-decoration: none">Metodi di Pagamento</a> |
                 <a href="scripts/logout.php" style="text-decoration: none">Logout</a> |
                 <a href="scripts/delete_account.php" style="color: red; text-decoration: none">Elimina account</a>
             </nav>
+        </div>
+
+        <div class="w3-container w3-padding">
+
+            <h3>Cambio dati personali</h3>
+            <form class="w3-container" action="" method="post">
+                <label for="firstname">Nuovo Nome:</label><br>
+                <input class="w3-input w3-border w3-light-grey" type="text" id="firstname" name="firstname">
+                <label for="lastname">Nuovo Cognome:</label><br>
+                <input class="w3-input w3-border w3-light-grey" type="text" id="lastname" name="lastname">
+                <input class="w3-btn w3-blue" type="submit" value="Modifica Dati">
+            </form>
+
+            <h3>Cambio email</h3>
+            <form class="w3-container" action="" method="post">
+                <label for="email">Nuova Email:</label><br>
+                <input class="w3-input w3-border w3-light-grey" type="email" id="email" name="email">
+                <input class="w3-btn w3-blue" type="submit" value="Modifica Dati">
+            </form>
+
+            <h3>Cambio password</h3>
+            <form class="w3-container" action="" method="post">
+                <label for="pass">Nuova Password:</label><br>
+                <input class="w3-input w3-border w3-light-grey" type="password" id="pass" name="pass">
+                <input class="w3-btn w3-blue" type="submit" value="Modifica Dati">
+            </form>
+
         </div>
     </div>
 
--- shop.php
+++ shop.php
@@ -22,42 +22,42 @@
     </div>
 
     <div style="margin-bottom: 50px">
-        <div class="w3-row-padding" style="margin: 0 auto; width: 75%">
+        <div class="w3-row-padding product-container" style="margin: 0 auto;">
             <div class="w3-card w3-third w3-center w3-padding-32 product">
-                <img src="photos/shop_photos/bartop.png" alt="Bartop" style="width: 250px; height: 250px;"> <br>
+                <img src="photos/shop_photos/bartop.png" alt="Bartop"> <br>
                 <h3>Bartop</h3>
                 <h5>A partire da 400€</h5>
                 <button class="w3-btn w3-green" onclick="addedToCart(event)">Aggiungi al carrello</button>
             </div>
             <div class="w3-card w3-third w3-center w3-padding-32 product">
-                <img src="photos/shop_photos/fourplayer_cabinet.png" alt="Quattro Giocatori" style="width: 250px; height: 250px;"> <br>
+                <img src="photos/shop_photos/fourplayer_cabinet.png" alt="Quattro Giocatori"> <br>
                 <h3>Quattro Giocatori</h3>
                 <h5>A partire da 700€</h5>
                 <button class="w3-btn w3-green" onclick="addedToCart(event)">Aggiungi al carrello</button>
             </div>
             <div class="w3-card w3-third w3-center w3-padding-32 product">
-                <img src="photos/shop_photos/classic_cabinet.png" alt="Classico" style="width: 250px; height: 250px;"> <br>
+                <img src="photos/shop_photos/classic_cabinet.png" alt="Classico"> <br>
                 <h3>Classico</h3>
                 <h5>A partire da 600€</h5>
                 <button class="w3-btn w3-green" onclick="addedToCart(event)">Aggiungi al carrello</button>
             </div>
         </div>
 
-        <div class="w3-row-padding" style="margin: 20px auto; width: 75%">
+        <div class="w3-row-padding product-container" style="margin: 20px auto;">
             <div class="w3-card w3-third w3-center w3-padding-32 product">
-                <img src="photos/shop_photos/wallmounted_cabinet.png" alt="A Muro" style="width: 250px; height: 250px;"> <br>
+                <img src="photos/shop_photos/wallmounted_cabinet.png" alt="A Muro"> <br>
                 <h3>A muro</h3>
                 <h5>A partire da 500€</h5>
                 <button class="w3-btn w3-green" onclick="addedToCart(event)">Aggiungi al carrello</button>
             </div>
             <div class="w3-card w3-third w3-center w3-padding-32 product">
-                <img src="photos/shop_photos/racing_cabinet.png" alt="Racing" style="width: 250px; height: 250px;"> <br>
+                <img src="photos/shop_photos/racing_cabinet.png" alt="Racing"> <br>
                 <h3>Racing</h3>
                 <h5>A partire da 1000€</h5>
                 <button class="w3-btn w3-green" onclick="addedToCart(event)">Aggiungi al carrello</button>
             </div>
             <div class="w3-card w3-third w3-center w3-padding-32 product">
-                <img src="photos/shop_photos/custom_cabinet.png" alt="Personalizzato" style="width: 250px; height: 250px;"> <br>
+                <img src="photos/shop_photos/custom_cabinet.png" alt="Personalizzato"> <br>
                 <h3>Personalizzato</h3>
                 <h5>A partire da 0 a 2000€</h5>
                 <button class="w3-btn w3-green" onclick="addedToCart(event)">Aggiungi al carrello</button>
@@ -68,23 +68,18 @@
     <script>
     function addedToCart(event) {
         // Controllo se l'utente sta utilizzando un dispositivo mobile
-        if(window.innerWidth <= 768) {
-            // Se sì, reindirizza all'altra pagina
-            window.location.href = "buy.php";
-        } else {
-            // Se no, esegui il codice già presente
-            const popup = document.getElementById("buyPopup");
-            const overlay = document.getElementById("overlay");
-            popup.style.display = "block";
-            overlay.style.display = "block";
+        // Se no, esegui il codice già presente
+        const popup = document.getElementById("buyPopup");
+        const overlay = document.getElementById("overlay");
+        popup.style.display = "block";
+        overlay.style.display = "block";
 
-            document.documentElement.style.overflow = 'hidden';
-            document.body.style.overflow = 'hidden';
-            document.documentElement.style.height = '100%';
-            document.body.style.height = '100%';
-            document.documentElement.style.margin = '0';
-            document.body.style.margin = '0';
-        }
+        document.documentElement.style.overflow = 'hidden';
+        document.body.style.overflow = 'hidden';
+        document.documentElement.style.height = '100%';
+        document.body.style.height = '100%';
+        document.documentElement.style.margin = '0';
+        document.body.style.margin = '0';
     }
 </script>
 
